80fb9a5eee29e948cff83b041af2137f996adeab
Miihsoo <yasminrochachagas2006@gmail.com>
2025-07-21 13:28:52 -0400

    fix: img fundo sicad

--- plataforma-cursos-react/src/components/Footer.jsx
+++ plataforma-cursos-react/src/components/Footer.jsx
@@ -6,6 +6,7 @@ export default function Footer() {
   return (
     <footer>
       <div className="container">
+        <p className="Link">Desenvolvido pela Gerência de Tecnologia da Informação - SEJUSC</p>
         <p>&copy; 2025 SICAD Todos os direitos reservados.</p>
       </div>
     </footer>
